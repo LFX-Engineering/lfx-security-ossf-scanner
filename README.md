219cc3f9ccb456fbc838b58f66f8904309659dae
David Deal <dealako@gmail.com>
2021-10-07 11:35:29 -0700

    Updated Serverless Deployment Configuration
    Signed-off-by: David Deal <dealako@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
-# lfx-security-ossf-scanner
-LFX Security OSSF Scanner
+# LFX Security OSSF Scanner
+
+This tool wraps the OSSF score tool as an AWS lambda so that we can generate the report for each active project in LFX
+Security.
+
+## Deployment
+
+```bash
+yarn deploy:dev
+```
+
+## Command Line Testing
+
+```bash
+# First: Log into your AWS account for the appropriate environment
+# Second: invoke using the desired payload, adjust the target repository and provide a GitHub authorization token value
+aws --region us-east-2 lambda invoke \
+  --function-name lfx-securitry-ossf-scanner-dev-ossf-scanner \
+  --cli-binary-format raw-in-base64-out \
+  --payload '{"repository":"github.com/communitybridge/easycla", "github_auth_token":"ghs_XXXX..."}' \
+  out.txt
+```
 
 ## References
 
-- 
+- [Open Source Project Criticality Score - GitHub](https://github.com/ossf/criticality_score)
+- [LFX Security API](https://github.com/LF-Engineering/lfx-security)
+- [LFX Security Console](https://github.com/LF-Engineering/vulnerability-detection)
+
+## License
+
+Copyright The Linux Foundation and each contributor to LFX.
+
+This project’s source code is licensed under the MIT License. A copy of the license is available in LICENSE.
+
+The project includes source code from keycloak, which is licensed under the Apache License, version 2.0 \(Apache-2.0\), a copy of which is available in LICENSE-keycloak.
+
+This project’s documentation is licensed under the Creative Commons Attribution 4.0 International License \(CC-BY-4.0\). A copy of the license is available in LICENSE-docs.
--- main.py
+++ main.py
@@ -3,25 +3,53 @@
 # Copyright The Linux Foundation and each contributor to CommunityBridge.
 # SPDX-License-Identifier: MIT
 import json
+import os
+from datetime import datetime
 
 from criticality_score import run
 
 
 def lambda_handler(event, context):
+    start_time = datetime.now()
     fn = context["function_name"]
+
+    # Check the input - make sure we have everything
+    if 'github_auth_token' not in event:
+        print(f'unable to generate criticality score report - missing github_auth_token from event data')
+        return
+    if 'repository' not in event:
+        print(f'unable to generate criticality score report - missing target repository from event data')
+        return
+
+    # Set this value in the environment
+    os.environ["GITHUB_AUTH_TOKEN"] = event['github_auth_token']
+
+    # extract our repository from the event data
     url = event["repository"]
     print(f'{fn} - processing repository: {url}')
+
     repo = run.get_repository(url)
     output = run.get_repository_stats(repo, [])
     print(json.dumps(output, indent=4))
+
+    # TODO: add to repository_statistics table
+    # How to add to the DB?
+    # 1. Direct DB Insert
+    # 2. Send AWS SNS Message, but this seems overkill, not necessary
+    # 3. Send API request via HTTPS, then API will add to the DB for me...
+    print(f'Finished processing - duration: {datetime.now() - start_time}')
 
 
 def main():
     # invoked from the command line
 
+    if 'GITHUB_AUTH_TOKEN' not in os.environ:
+        print(f'missing GITHUB_AUTH_TOKEN environment variable')
+
     # Event data for the lambda
     event = {
-        "repository": "github.com/communitybridge/easycla"
+        "repository": "github.com/communitybridge/easycla-contributor-console",
+        "github_auth_token": os.environ['GITHUB_AUTH_TOKEN']
     }
 
     # Context data for the lambda
