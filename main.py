8a98bad1d9af182c0908a33edaa6ef7d508650d0
David Deal <dealako@gmail.com>
2021-10-20 11:37:31 -0700

    Resoved Event Decoding Issue
    Signed-off-by: David Deal <dealako@gmail.com>

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import requests
 from criticality_score import run
 
 
-def validate_input(event: Dict[str, Any]) -> bool:
+def validate_input(event_body: Dict[str, Any]) -> bool:
     fn = 'validate_input'
 
     valid = True
@@ -33,34 +33,34 @@ def validate_input(event: Dict[str, Any]) -> bool:
     if 'AUTH0_PLATFORM_AUDIENCE' not in os.environ:
         print(f'missing AUTH0_PLATFORM_AUDIENCE environment variable')
         valid = False
-    if 'github_auth_token' not in event['body']:
+    if 'github_auth_token' not in event_body:
         print(f'{fn} - unable to generate criticality score report - missing github_auth_token from event data')
         valid = False
-    if event['body']['github_auth_token'] == '':
+    if event_body['github_auth_token'] == '':
         print(f'{fn} - unable to generate criticality score report - github_auth_token value is empty')
         valid = False
-    if 'repository_url' not in event['body']:
+    if 'repository_url' not in event_body:
         print(f'{fn} - unable to generate criticality score report - missing target repository_url from event data')
         valid = False
-    if event['body']['repository_url'] == '':
+    if event_body['repository_url'] == '':
         print(f'{fn} - unable to generate criticality score report - repository_url value is empty')
         valid = False
-    if 'repository_id' not in event['body']:
+    if 'repository_id' not in event_body:
         print(f'{fn} - unable to generate criticality score report - missing target repository_url from event data')
         valid = False
-    if event['body']['repository_id'] == '':
+    if event_body['repository_id'] == '':
         print(f'{fn} - unable to generate criticality score report - repository_id value is empty')
         valid = False
-    if 'project_id' not in event['body']:
+    if 'project_id' not in event_body:
         print(f'{fn} - unable to generate criticality score report - missing project_id from event data')
         valid = False
-    if event['body']['project_id'] == '':
+    if event_body['project_id'] == '':
         print(f'{fn} - unable to generate criticality score report - project_id value is empty')
         valid = False
-    if 'project_sfid' not in event['body']:
+    if 'project_sfid' not in event_body:
         print(f'{fn} - unable to generate criticality score report - missing project_sfid from event data')
         valid = False
-    if event['body']['project_sfid'] == '':
+    if event_body['project_sfid'] == '':
         print(f'{fn} - unable to generate criticality score report - project_sfid value is empty')
         valid = False
 
@@ -156,27 +156,26 @@ def lambda_handler(event: Dict[str, Any], context) -> Dict[str, Any]:
         print(f'{fn} - received event with no body - unable to process message')
         return {}
 
-    # json_data = json.dumps(event)
-    print(f'{fn} - event {type(event)}: {event}')
-    print(f'{fn} - event {type(dict(event))}: {dict(event)}')
+    event_body = json.loads(event['body'])
+    print(f'{fn} - event {type(event_body)}: {event_body}')
 
     # Check the input - make sure we have everything
-    if not validate_input(dict(event)):
+    if not validate_input(event_body):
         return {}
 
     # Set this value in the environment
-    os.environ["GITHUB_AUTH_TOKEN"] = event['body']['github_auth_token']
+    os.environ["GITHUB_AUTH_TOKEN"] = event_body['github_auth_token']
 
     stage = os.environ['STAGE']
 
     # extract our repository from the event data
-    url = event['body']['repository_url']
+    url = event_body['repository_url']
     url = re.sub(r'^https?://', '', url, flags=re.IGNORECASE)
 
-    project_id = event['body']['project_id']
-    project_sfid = event['body']['project_sfid']
-    project_name = event['body']['project_name']
-    repository_id = event['body']['repository_id']
+    project_id = event_body['project_id']
+    project_sfid = event_body['project_sfid']
+    project_name = event_body['project_name']
+    repository_id = event_body['repository_id']
     print(f'{fn} - {stage} - processing repository: {url}')
 
     try:
@@ -205,29 +204,31 @@ def main():
         print(f'missing GITHUB_AUTH_TOKEN environment variable')
 
     # Event data for the lambda - below are test/junk values
-    event = {
-        # Consider adding additional meta-data for this message
-        # See: https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html
-        "body": {
-            "id": "38e2ef0e-1983-4444-aaaa-222222222222",
-            "type": "ossf_scan",
-            "version": "v1",
-            "project_id": "38e2ef0e-1983-4c92-a9f0-98255cd61af1",
-            "project_sfid": "a092M000000000000000000000",
-            "project_name": "Foo",
-            "created_date_time": 0,
-            "repository_id": "435f5013-4406-4fcc-954c-zzzzzzzzzzzzzzz",
-            "repository_url": "https://github.com/communitybridge/easycla",
-            "github_auth_token": 'ghs_XYZ......',
-        }
-    }
+    # event = {
+    #     # Consider adding additional meta-data for this message
+    #     # See: https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html
+    #     "body": {
+    #         "id": "38e2ef0e-1983-4444-aaaa-222222222222",
+    #         "type": "ossf_scan",
+    #         "version": "v1",
+    #         "project_id": "38e2ef0e-1983-4c92-a9f0-98255cd61af1",
+    #         "project_sfid": "a092M000000000000000000000",
+    #         "project_name": "Foo",
+    #         "created_date_time": 0,
+    #         "repository_id": "435f5013-4406-4fcc-954c-zzzzzzzzzzzzzzz",
+    #         "repository_url": "https://github.com/communitybridge/easycla",
+    #         "github_auth_token": 'ghs_XYZ......',
+    #     }
+    # }
+    event2 = {
+        'body': '{"id":"testtest-5ce6-4558-8d6b-abad9e82a7fd","type":"ossf_scan","version":"v1","project_sfid":"a092M00001IV7AiQAL","project_id":"testtest-1983-4c92-a9f0-98255cd61af1","project_name":"EasyCLA","created_datetime":1634753930,"repository_id":"testtest-1bd8-430e-953e-0f5f0a369d03","repository_url":"https://github.com/communitybridge/docs","github_auth_token":"ghs_XXXXXXXXXXXXXX"}'}
 
     # Context data for the lambda
     # see: https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
     context = {
         "function_name": "lfx-security-ossf-scanner"
     }
-    lambda_handler(event, context)
+    lambda_handler(event2, context)
 
 
 if __name__ == "__main__":
